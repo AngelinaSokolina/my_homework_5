b4c9720ab73d46efd5e47f758656e41e47a4b765
Your Name <akusilda@proton.me>
2026-04-01 23:15:55 +0300

    Переделала тест personal, сделав его более универсальным. Добавила тест test_personal

--- financial_transactions/personal.py
+++ financial_transactions/personal.py
@@ -1,15 +1,20 @@
 import pandas as pd
-
+from pathlib import Path
 
 """Пользователь вводит свой id, чтобы посмотреть информацию о счете"""
 
-df = pd.read_excel('transactions_excel.xlsx')
-
+df = pd.read_excel(Path(__file__).parent / 'transactions_excel.xlsx')
 
 def speak_question()-> None:
-    question = int(input("Если вы хотите узнать информацию о счете, введите свой id: "))
-    mask = df['id'] == question
-    if mask.any():
-        print(df.loc[mask])
-    else:
-        print('Извините, введенный id не найден')
+    while True:
+        question = input("Если вы хотите узнать информацию о счете, введите свой id: ")
+        try:
+            user_id = int(question)
+            mask = df['id'] == user_id
+            if mask.any():
+                print(df.loc[mask])
+                break
+            else:
+                print('Извините, введенный id не найден')
+        except ValueError:
+            print('Ввод неверный! Введите только цифры без пробелов')
